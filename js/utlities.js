7b96f4ada50af0e44b0679fa4a1e3bbf1f9447d4
Ashaduzzaman10 <infoaj549814@gmail.com>
2023-09-14 03:06:44 +0600

    calculation-part done

--- js/utlities.js
+++ js/utlities.js
@@ -1,21 +1,25 @@
 // initial math for all 
 function CalculateInputResult(elementId1, elementId2) {
   const base = document.getElementById(elementId1);
-  const baseNumber = parseInt(base.value);
+  const baseString = base.value;
+  const baseNumber = parseFloat(baseString);
   const height = document.getElementById(elementId2);
-  const heightNumber = parseInt(base.value);
-  const sum = parseInt(( 0.5 * baseNumber * heightNumber));
+  const heightString = height.value;
+  const heightNumber = parseFloat(baseString);
+  const sum = ( 0.5 * baseNumber * heightNumber);
   return sum;
 }
 
 // initial math for all 
 function CalculateInputResult1(elementId1, elementId2) {
-  const base = document.getElementById(elementId1);
-  const baseNumber = parseInt(base.value);
-  const height = document.getElementById(elementId2);
-  const heightNumber = parseInt(base.value);
-  const sum = parseInt(( baseNumber * heightNumber));
-  return sum;
+   const base = document.getElementById(elementId1);
+   const baseString = base.value;
+   const baseNumber = parseFloat(baseString);
+   const height = document.getElementById(elementId2);
+   const heightString = height.value;
+   const heightNumber = parseFloat(baseString);
+   const sum =  baseNumber * heightNumber;
+   return sum;
 }
 // printing result 
 function printingResult(elementId,TotalSum) {
